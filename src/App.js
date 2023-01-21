88140ce339b86106029f538aa4794f90e8a0f49b
shane0 <shane.null@gmail.com>
2023-01-21 13:50:26 -0800

    'Fixes #8'

--- src/App.js
+++ src/App.js
@@ -13,9 +13,31 @@ function App() {
         <Row>
           <Col>
             <h2>workout log</h2>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSccRVwwl4ciW-j9FXxWpK1BOgalhIlPKjlpxr5yJg5UKaO6Rw/viewform?vc=0&c=0&w=1&flr=0">
+              workout
+            </a>
+            <br />
+            <a href="https://docs.google.com/spreadsheets/d/1kKEBMBDIk7f7lX3Yt88wA3drSodxAd-WHtbQuUGkEWY/edit?resourcekey#gid=1895309793">
+              history
+            </a>
           </Col>
           <Col>
             <h2>bar loader</h2>
+            <a href="https://replit.com/@zoo00/barloader#index.js">replit</a>
+            <hr />
+            <ul>
+              <li>DEADLIFT = 70,35,5,2.5</li>
+              <li>SQUAT = 45,10,10,2.5</li>
+              <li>OVERHead = 35,2.5</li>
+              <li>BENCH = 35,5,2.5</li>
+            </ul>
+            <hr />
+            <iframe
+              frameborder="0"
+              width="100%"
+              height="500px"
+              src="https://replit.com/@zoo00/barloader?embed=true"
+            ></iframe>
           </Col>
           <Col>
             <h2>manage gyms</h2>
